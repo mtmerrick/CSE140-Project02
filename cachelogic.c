bb16cb7c270b535ffb56d646eb2ec5dba701a23c
rhules <38119764+rhules@users.noreply.github.com>
2018-11-04 18:02:13 -0800

    0.0.0.6-2~2#

--- cachelogic.c
+++ cachelogic.c
@@ -220,7 +220,7 @@ void accessMemory(address addr, word* data, WriteEnable we) {
 	highlight_block(index ,aNum);
 	highlight_offset(index, aNum, offset, MISS);
 	//copy block from memory
-	memcpy(temp, (word*)addr, 4);
+	memcpy(temp->data[0], (word*)addr, (size_t)block_size);
 	if(we == READ){
 		data = (word*)&(temp->data[offset]);
 	}

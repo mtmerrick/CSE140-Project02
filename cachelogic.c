0ca293586f78f4aab567ae0af17fda8e493b0195
rhules <38119764+rhules@users.noreply.github.com>
2018-11-04 19:01:00 -0800

    0.0.1-2~1#

--- cachelogic.c
+++ cachelogic.c
@@ -159,12 +159,14 @@ void accessMemory(address addr, word* data, WriteEnable we) {
 		mask += 1 << (32 - i); //pow(2,32-(int)i);
 	}
 	tag = addr & mask;
+	tag = tag >> (32 - tag_bits);
 	//get index
 	mask = 0;
 	for(int i = 0; i < index_bits; i++){
 		mask += 1 << (32 - tag_bits - i); //pow(2, 32 - (int)tag_bits - (int)i);
 	}
 	index = addr & mask;
+	index = index >> (32 - index_bits);
 	//get offset
 	mask = 0;
 	for(int i = 0; i < offset_bits; i++){
